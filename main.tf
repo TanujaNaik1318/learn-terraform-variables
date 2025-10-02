28f190a331d149c02ba86c98c19f3a72d6298168
Tanuja <you@example.com>
2025-10-02 07:50:20 -0700

    test

--- main.tf
+++ main.tf
@@ -1,12 +1,19 @@
 terraform {
+  cloud {
+    organization = "policy-as-code-training"
+    workspaces {
+      name = "policy-dev-tn"
+      project = "policy-as-code"
+    }
+  }
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
     }
   }
 }
 
-provider "aws" {
+ provider "aws" {
   region  = "us-west-1"
 }
 
@@ -109,13 +116,11 @@ module "elb_http" {
 module "ec2_instances" {
   source = "./modules/aws-instance"
 
-  instance_count     = 2
-  instance_type      = "t2.micro"
+  instance_count = var.instance_count
+  instance_type  = var.instance_type
+
   subnet_ids         = module.vpc.private_subnets[*]
   security_group_ids = [module.app_security_group.this_security_group_id]
 
-  tags = {
-    project     = "project-alpha",
-    environment = "dev"
-  }
+  
 }
